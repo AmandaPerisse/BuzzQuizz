01c99864d323a6c85d98fb0760dd761ea28ec1e7
Amanda <amandaveras1999@gmail.com>
2021-11-18 16:46:54 -0300

    Começando a tela de um quizz especifico

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let allQuizzes = document.querySelector(".indexAllQuizzesList");
+let index = document.querySelector(".index");
+let specifiedQuizz = document.querySelector(".OpenedQuizz");
 
 let promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
 promise.then(getQuizzes);
@@ -8,9 +10,24 @@ function getQuizzes(resposta){
     console.log(resposta.data);
     for(let i = 0; i < resposta.data.length; i++){
         allQuizzes.innerHTML += `
-        <button style = "background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${resposta.data[i].image});" class = "AllQuizzesQuizz">
+        <button onclick = "searchQuizz(${resposta.data[i].id});" style = "background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${resposta.data[i].image});" class = "AllQuizzesQuizz">
             <p>${resposta.data[i].title}</p>
         </button>
         `;
     }
+}
+
+function searchQuizz(identificador){
+    let promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/'+identificador);
+    promise.then(openQuizz);
+}
+
+function openQuizz(resposta){
+    index.classList.add("escondido");
+    specifiedQuizz.innerHTML = "";
+    specifiedQuizz.innerHTML += `
+    <div style = "background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), url(${resposta.data.image});" class = "OpenedQuizzHeader">
+        <h4>${resposta.data.title}</h4>
+    </div>
+    `;
 }
