930e76790427b67a0c0330e69aa8629c8509b98c
kassiajrauen <kassiajrauen@gmail.com>
2021-11-22 12:35:12 -0300

    Criação do Quizz: Níveis do quizz

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ let levels = [];
 let idQuizz = 0;
 let payload = {};
 let qtdPerg;
+let qtdNiveis;
 
 let promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
 promise.then(getQuizzes);
@@ -176,7 +177,7 @@ function makeQuestions() {
     const title = document.querySelector('input[name=titulo]').value;
     const url = document.querySelector('input[name=url]').value;
     qtdPerg = document.querySelector('input[name=qtdPerg]').value;
-    const qtdNiveis = document.querySelector('input[name=qdtNiveis]').value;
+    qtdNiveis = document.querySelector('input[name=qdtNiveis]').value;
 
     const validateTitle = title.length >= 20 && title.length <= 65;
     const validateUrl = isValidUrl(url);
@@ -213,6 +214,17 @@ function makeQuestions() {
             <input class="input-quizz" type="text" name="urlAnswers3-quest${i + 1}" placeholder="URL da imagem 3">
         `
     }
+
+    let levels = document.querySelector('.page-3 .info-quizz')
+    for (let i = 0; i < parseInt(qtdNiveis); i++) {
+        levels.innerHTML += `
+            <h7>Nível ${i + 1}</h7>
+            <input class="input-quizz" type="text" name="title-level${i + 1}" placeholder="Título do nível">
+            <input class="input-quizz" type="text" name="percent-level${i + 1}" placeholder="% de acerto mínima">
+            <input class="input-quizz" type="text" name="url-level${i + 1}" placeholder="URL da imagem do nível">
+            <input class="input-quizz" type="text" name="descrition-level${i + 1}" placeholder="Descrição do nível">
+        `
+    }
 }
 
 function isHexColor(hex) {
@@ -242,7 +254,7 @@ function makeLevels(passPage2) {
         const validateAnswers = correctAnswers != undefined && incorrectAnswers1 != undefined || incorrectAnswers2 != undefined || incorrectAnswers3 != undefined;
         const validateUrl = isValidUrl(urlAnswers) && isValidUrl(urlAnswers1) && isValidUrl(urlAnswers2) && isValidUrl(urlAnswers3);
 
-        // if (!validateTitle && !validateColor && !validateAnswers && !validateUrl) {
+        // if (!validateTitle || !validateColor || !validateAnswers || !validateUrl) {
         //     alert('Ops, algo deu errado! Preencha os dados novamente ;)');
         //     validate = false;
         //     break;
@@ -298,13 +310,48 @@ function makeLevels(passPage2) {
 }
 
 function finishCreateQuizz(passPage3) {
-    const title = document.querySelector('input[name=titulo]').value;
-    const url = document.querySelector('input[name=url]').value;
-    const qtdPerg = document.querySelector('input[name=qtdPerg]').value;
-    const qtdNiveis = document.querySelector('input[name=qdtNiveis]').value;
+    let validate = true;
+    let listPercent = [];
+    let listLevels = [];
 
-    // if()
-    {
+    for (let i = 0; i < parseInt(qtdNiveis); i++) {
+        const title = document.querySelector(`input[name=title-level${i + 1}]`).value;
+        const percent = document.querySelector(`input[name=percent-level${i + 1}]`).value;
+        const url = document.querySelector(`input[name=url-level${i + 1}]`).value;
+        const descrition = document.querySelector(`input[name=descrition-level${i + 1}]`).value;
+
+        const validateTitle = title.length >= 10;
+        console.log("🚀 ~ file: script.js ~ line 322 ~ finishCreateQuizz ~ validateTitle", validateTitle)
+        const validatePercent = parseInt(percent) >= 0 && parseInt(percent) <= 100;
+        console.log("🚀 ~ file: script.js ~ line 324 ~ finishCreateQuizz ~ validatePercent", validatePercent)
+        const validateUrl = isValidUrl(url);
+        console.log("🚀 ~ file: script.js ~ line 326 ~ finishCreateQuizz ~ validateUrl", validateUrl)
+        const validateDescrition = descrition.length >= 30;
+        console.log("🚀 ~ file: script.js ~ line 328 ~ finishCreateQuizz ~ validateDescrition", validateDescrition)
+
+        if (!validateTitle || !validatePercent || !validateUrl || !validateDescrition) {
+            alert('Ops, algo deu errado! Preencha os dados novamente ;)');
+            validate = false;
+            break;
+        }
+
+        listPercent.push(percent);
+
+        listLevels.push({
+            title: title,
+            image: url,
+            text: descrition,
+            // minValue: percent,
+        })
+    }
+
+    if (!listPercent.includes("0")) {
+        alert("Pelo menos uma porcentagem de acerto deve ser 0%")
+        validate = false;
+    }
+
+    if (validate) {
+        payload = { ...payload, levels: listLevels };
         console.log('funcionando');
         const passPage = document.querySelector('.page-3');
         passPage.classList.add('escondido');
@@ -334,11 +381,7 @@ function accessQuizz(AccessQuizz) {
 
 function backHome(backHome) {
     console.log('funcionando');
-    const passPage = document.querySelector('.page-4');
-    passPage.classList.add('escondido');
-
-    const newPage = document.querySelector('.container');
-    newPage.classList.remove('escondido');
+    document.location.reload(true)
 }
 
 // function armazenarInfosQuizz(quizzInfos) {
